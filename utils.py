63fbe201157d3f77175c2436fa0b5129a8870f3d
kozistr <kozistr@gmail.com>
2019-05-12 16:14:03 +0900

    feat: implement image loaders

---
+++ utils.py
@@ -0,0 +1,50 @@
+import os
+import cv2
+import numpy as np
+
+from glob import glob
+from tqdm import tqdm
+
+
+def load_image_from_file(fn, mode='rgb', normalize=False, norm_scale="0,1"):
+    """ loading an image from file name
+    :param fn: str. file name
+    :param mode: str. mode to load image
+    :param normalize: bool.
+    :param norm_scale: str. range of image pixel value to normalize
+    :return: np.array.
+    """
+    assert mode in ('rgb', 'grayscale')
+    assert norm_scale in ("0,1", "-1,1")
+
+    img = cv2.imread(fn, cv2.IMREAD_COLOR if mode == 'rgb' else cv2.IMREAD_GRAYSCALE)
+
+    if normalize:
+        if norm_scale == "0,1":
+            img /= 255.
+        else:
+            img = (img / 127.5) - 1.
+    return img
+
+
+def load_images_from_path(path, ext="png", mode='rgb', normalize=False, norm_scale="0,1"):
+    """ loading an image from file name
+    :param path: str. file path
+    :param ext: str. extension
+    :param mode: str. mode to load image
+    :param normalize: bool.
+    :param norm_scale: str. range of image pixel value to normalize
+    :return: list of np.array.
+    """
+    assert mode in ('rgb', 'grayscale')
+    assert norm_scale in ("0,1", "-1,1")
+
+    _files = glob(os.path.join(path, "*.{}".format(ext)))
+
+    images = [load_image_from_file(_file, mode, normalize, norm_scale)
+              for _file in tqdm(_files)]
+    return np.asarray(images)
+
+
+if __name__ == "__main__":
+    pass
