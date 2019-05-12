b95ffacb112ef2c633bb1643952bfa66e7de3175
kozistr <kozistr@gmail.com>
2019-05-12 16:19:26 +0900

    feat: support both 'rgb', 'bgr' modes

--- utils.py
+++ utils.py
@@ -12,12 +12,15 @@ def load_image_from_file(fn, mode='rgb', normalize=False, norm_scale="0,1"):
     :param mode: str. mode to load image
     :param normalize: bool.
     :param norm_scale: str. range of image pixel value to normalize
-    :return: np.array.
+    :return: np.array. rgb image
     """
-    assert mode in ('rgb', 'grayscale')
+    assert mode in ('rgb', 'bgr', 'grayscale')
     assert norm_scale in ("0,1", "-1,1")
 
-    img = cv2.imread(fn, cv2.IMREAD_COLOR if mode == 'rgb' else cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(fn, cv2.IMREAD_COLOR if not mode == "grayscale" else cv2.IMREAD_GRAYSCALE)
+
+    if mode == "rgb":
+        img = img[::-1]
 
     if normalize:
         if norm_scale == "0,1":
@@ -41,9 +44,9 @@ def load_images_from_path(path, ext="png", mode='rgb', normalize=False, norm_sca
 
     _files = glob(os.path.join(path, "*.{}".format(ext)))
 
-    images = [load_image_from_file(_file, mode, normalize, norm_scale)
-              for _file in tqdm(_files)]
-    return np.asarray(images)
+    images = np.asarray([load_image_from_file(_file, mode, normalize, norm_scale)
+                         for _file in tqdm(_files)])
+    return images
 
 
 if __name__ == "__main__":
