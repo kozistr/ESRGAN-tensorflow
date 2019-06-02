613e00e52d6bbf0d93df233c851a79d20184591b
kozistr <kozistr@gmail.com>
2019-06-02 18:09:24 +0900

    feat: impl pixel-shuffle

--- tfutils.py
+++ tfutils.py
@@ -192,6 +192,20 @@ def dense(x, units: int,
         return x
 
 
+def sub_pixel_conv2d(x, f, s: int = 2):
+    """reference : https://github.com/tensorlayer/SRGAN/blob/master/tensorlayer/layers.py"""
+
+    if f is None:
+        f = int(int(x.get_shape()[-1]) / (s ** 2))
+
+    _, a, b, c = x.get_shape().as_list()
+    bsize = tf.shape(x)[0]
+
+    x_s = tf.split(x, s, 3)
+    x_r = tf.concat(x_s, 2)
+    return tf.reshape(x_r, (bsize, s * a, s * b, f))
+
+
 def spectral_norm(w, iteration: int = 1):
     w_shape = w.shape.as_list()
 
